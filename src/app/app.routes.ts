2886736f5d72d3fe15eb631b1deed142698f90ba
Fabio <fabio04musitelli@gmail.com>
2025-03-20 15:40:50 +0100

    ✨ add orders feature with routing and initial component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'orders',
+        loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersRoutingModule)
+    }
+];
