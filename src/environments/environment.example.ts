24ad715646885886a0bd4acc335e4acfb34e42eb
Fabio <fabio04musitelli@gmail.com>
2025-03-20 14:42:59 +0100

    ✨ added environments, added example env

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+// ! this is an example file, you should create a new file with the same name without the .example extension
+
+export const environment = {
+    apiUrl: 'your-api-url',
+    production: true
+};
