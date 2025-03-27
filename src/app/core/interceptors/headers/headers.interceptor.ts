859fae111368fffffe7ccbe48664992cf9e710a4
Giacomo Cattani <giacomo.cattani04@gmail.com>
2025-03-27 14:35:59 +0100

    ✨ fix typo in app.component.ts styleUrl property

--- src/app/core/interceptors/headers/headers.interceptor.ts
+++ src/app/core/interceptors/headers/headers.interceptor.ts
@@ -12,24 +12,23 @@ import { TokenService } from '../../services/token.service';
 @Injectable()
 export class HeadersInterceptor implements HttpInterceptor {
 
-  constructor(private tokenService: TokenService, private masterKey: boolean) {}
+  constructor(private tokenService: TokenService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.tokenService.getToken();
-    const masterkey = this.masterKey && environment.masterKey;
 
     let modifiedRequest;
 
     if (!token) {
       modifiedRequest = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${masterkey}`
+          Authorization: `Bearer ${environment.masterKey}`
         }
       });
-    } else if (masterkey) {
+    } else if (environment.masterKey) {
       modifiedRequest = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${masterkey}`
+          Authorization: `Bearer ${environment.masterKey}`
         }
       });
     }
