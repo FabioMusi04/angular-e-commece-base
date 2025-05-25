1a052dba0d092c8558745e9a6ba31dee5a96e8c7
Federico Balducci <ott8vv@gmail.com>
2025-05-25 22:23:10 +0200

    feat(users): add IUser interface for user management

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,16 @@
+export interface IUser{
+  _id?: string;
+  name: string;
+  email: string;
+  password?: string;
+  role?: 'user' | 'admin';
+  lastLogin?: Date;
+  device: string[]
+  preLastLogin?: Date;
+  isConfirmed?: boolean;
+  isEnabled?: boolean;
+  isDeleted?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  __v?: number;
+}
